e3c9f5ab9be5977c48a4a401bf2f1a778527c32f
Ville Puuska <puuskaville@gmail.com>
2023-11-25 13:08:23 +0200

    start script for copying template notebook and getting input for the day

---
+++ get_input.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+    echo "Pass the year and day as parameters."
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Missing folder for the year."
+    exit 1
+fi
+
+if [ ! -d "$1/data" ]; then
+    echo "Missing data folder for the year."
+    exit 1
+fi
+
+if [ -f "$1/day$2.ipynb" ]; then
+    echo "Notebook for the day already exists."
+else
+    echo "Copying template notebook."
+    cp "template.ipynb" "$1/day$2.ipynb"
+fi
+
+if [ -f "$1/data/day$2.txt" ]; then
+    echo "Input for the day already exists."
+else
+    echo "Curling input."
+    # TODO: Actually get the input.
+fi
