95c24d5682b456c3ff52bcfbc18f5bde4443eeee
Ville Puuska <puuskaville@gmail.com>
2025-12-01 20:37:16 +0200

    2025 day1

--- get_input.sh
+++ get_input.sh
@@ -24,13 +24,17 @@ fi
 
 if [ -f "$1/data/day$2.txt" ]; then
     echo "Input for the day already exists."
-elif [ -z "$AOC_SESSION" ]; then
-    echo "You need to set the session cookie to the environment variable AOC_SESSION."
-    exit 1
 else
-    echo "Curling input."
-    curl -A "github.com/VillePuuska/AoC" --cookie "session=$AOC_SESSION" https://adventofcode.com/$1/day/$2/input > "$1/data/day$2.txt"
+    echo "Making empty input file."
+    touch "$1/data/day$2.txt"
 fi
+# elif [ -z "$AOC_SESSION" ]; then
+#     echo "You need to set the session cookie to the environment variable AOC_SESSION."
+#     exit 1
+# else
+#     echo "Curling input."
+#     curl -A "github.com/VillePuuska/AoC" --cookie "session=$AOC_SESSION" https://adventofcode.com/$1/day/$2/input > "$1/data/day$2.txt"
+# fi
 
 if [ -f "$1/data/day$2_ex.txt" ]; then
     echo "Example file for the day already exists."
